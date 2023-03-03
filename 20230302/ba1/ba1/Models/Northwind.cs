992d2c0cf85fc559920adfed6a205a141cad4148
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-03-02 21:11:06 -0600

    Update

---
+++ 20230302/ba1/ba1/Models/Northwind.cs
@@ -0,0 +1,7 @@
+﻿namespace ba1.Models
+{
+    public class Northwind
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
---
+++ 20230302/ba1/ba1/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace ba1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = "";
+        public short SupplierID { get; set; }
+        public short CategoryID { get; set; }
+        public string QuantityPerUnit { get; set; } = "";
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+        public short ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+
+        //"id": 1,
+        //"productName": "Chai",
+        //"supplierID": 1,
+        //"categoryID": 1,
+        //"quantityPerUnit": "10 boxes x 20 bags",
+        //"unitPrice": 18,
+        //"unitsInStock": 39,
+        //"unitsOnOrder": 0,
+        //"reorderLevel": 10,
+        //"discontinued": false
+    }
+}
