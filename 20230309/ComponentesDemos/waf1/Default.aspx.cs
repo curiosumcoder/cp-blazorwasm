dca585aa47d0668ca985a6ddfc613b17250158b7
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-03-10 12:13:31 -0600

    Update

---
+++ 20230309/ComponentesDemos/waf1/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace waf1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Unnamed1_SelectionChanged(object sender, EventArgs e)
+        {
+            lMain.Text = $"¡Fecha cambió! {cMain.SelectedDate.ToShortDateString()}";
+        }
+    }
+}
